cf21dd544a76c5583a2a26ce3c9cb23d75abb63d
Liuying Wang <wlyemail@gmail.com>
2020-05-28 19:48:41 -0400

    completed LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Liuying Wang
  */
 public class LeapYear {
 
@@ -14,11 +14,16 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year){
+		return ((year%400 == 0)||(year%4 == 0 && year%100 != 0));
+		}
+
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
+            System.out.println("e.g. java LeapYear 2000");
         }
         for (int i = 0; i < args.length; i++) {
             try {
